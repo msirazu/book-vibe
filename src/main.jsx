fd5caa08fcba6b5787b9d5dee98c1ec873266a89
msirazu <thebdrazu@gmail.com>
2026-03-19 19:44:03 +0600

    project stated. tailwind, daisyui, react router, lucide icons installed. folder structured completed.

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './assets/styles/index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes'
+import { HelmetProvider } from 'react-helmet-async'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>,
+)
