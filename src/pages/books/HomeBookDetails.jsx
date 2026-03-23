a9efdc93670ff639cf8ee2e58239d66d7702c961
msirazu <thebdrazu@gmail.com>
2026-03-23 20:30:05 +0600

    read and wish list added with local storage save functionality

--- src/pages/books/HomeBookDetails.jsx
+++ src/pages/books/HomeBookDetails.jsx
@@ -2,12 +2,33 @@ import { useContext } from "react";
 import DataContext from "../../auth/context/DataContext";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { toast } from 'react-toastify';
+import { saveReadDataToLocalStorage, saveWishDataToLocalStorage } from "../../utilities/localstoragedb";
 
 const HomeBookDetails = () => {
     const books = useContext(DataContext);
     const { bookId } = useParams();
     const book = books.find(b => b.bookId === Number(bookId));
     const {bookName, author, image, category, publisher, rating, review, tags, totalPages, yearOfPublishing} = book;
+    
+    const handleRead = (id) => {
+        const isAdded = saveReadDataToLocalStorage(id);
+        if (isAdded) {
+            toast('Book Added to Read List Successfully')
+        } else {
+            toast('Book Already Exists in Read List')
+        }
+    }
+
+    const handleWish = (id) => {
+        const isAdded = saveWishDataToLocalStorage(id);
+         if (isAdded) {
+            toast('Book Added to Wish List Successfully')
+        } else {
+            toast('Book Already Exists in Wish List')
+        }
+    }
+
     return (
         <>
         <Helmet>
@@ -29,7 +50,7 @@ const HomeBookDetails = () => {
                     <hr className="border-dashed border-gray-300"/>
                     <p className="text-sm text-text"><span className="text-black font-bold">Review:</span> {review}</p>
                     <div>
-                    <p className="flex gap-5"><span className="font-bold">Tag: </span>{tags.map(tag => <span className="text-one">#{tag}</span>
+                    <p className="flex gap-5"><span className="font-bold">Tag: </span>{tags.map((tag, index) => <span key={index} className="text-one">#{tag}</span>
                     )}
                     </p>
                     
@@ -44,6 +65,10 @@ const HomeBookDetails = () => {
                     <p className="font-bold">{yearOfPublishing}</p>
                     <p className="text-text text-sm">Rating:</p>
                     <p className="font-bold">{rating}</p>
+                    </div>
+                    <div className="flex gap-5">
+                        <button onClick={() => handleRead(bookId)} className="btn">Read</button>
+                        <button onClick={() => handleWish(bookId)}  className="btn btn-two text-white">Wishlist</button>
                     </div>
                 </div>
             </section>
---
+++ src/utilities/localstoragedb.js
@@ -0,0 +1,43 @@
+const getReadFromLocalStorage = () => {
+    let cart = [];
+    const storedReadData = localStorage.getItem('bookVibeRead');
+    if (storedReadData) {
+        return cart = JSON.parse(storedReadData);
+    }
+    return cart;
+}
+
+const saveReadDataToLocalStorage = (id) => {
+    const storedRead = getReadFromLocalStorage();
+    const bookId = Number(id);
+    if (storedRead.includes(bookId)) {
+        return false;
+    }
+    storedRead.push(bookId);
+    const data = JSON.stringify(storedRead);
+    localStorage.setItem('bookVibeRead', data);
+    return true;
+}
+
+const getWishFromLocalStorage = () => {
+    let cart = [];
+    const storedReadData = localStorage.getItem('bookVibeWish');
+    if (storedReadData) {
+        return cart = JSON.parse(storedReadData);
+    }
+    return cart;
+}
+
+const saveWishDataToLocalStorage = (id) => {
+    const storedWish = getWishFromLocalStorage();
+    const bookId = Number(id);
+    if (storedWish.includes(bookId)) {
+        return false;
+    }
+    storedWish.push(bookId);
+    const data = JSON.stringify(storedWish);
+    localStorage.setItem('bookVibeWish', data);
+    return true;
+}
+
+export {getReadFromLocalStorage, saveReadDataToLocalStorage, saveWishDataToLocalStorage, getWishFromLocalStorage};
